9e5fd3ca5d2cb8a7b2c887a0ba2278ef3977f384
Joe Schaefer <joes@apache.org>
2010-03-24 18:51:41 +0000

    more validation
    git-svn-id: https://svn.apache.org/repos/asf/steve/trunk@1434190 13f79535-47bb-0310-9956-ffa450edef68

--- www/cgi-bin/cast-vote.pl
+++ www/cgi-bin/cast-vote.pl
@@ -3,20 +3,21 @@ use strict;
 use warnings;
 use CGI;
 use CGI::Carp qw/fatalsToBrowser/;
+use Digest::MD5;
 
 my $VOTE_TOOL = "/home/voter/bin/vote";
 my $VOTE_TMPDIR = "/home/voter/tmp";
-my $VOTE_DATADIR = "/home/voter/issues";
+my $VOTE_ISSUEDIR = "/home/voter/issues";
 
-$ENV{PATH_INFO} =~ m!^/([\w-]+)/([0-9a-f]{32})/(yna|stv[1-9]|select[1-9])$!
-    or die "Invalid path";
-my ($issue, $hash, $type) = ($1, $2, $3);
+$ENV{PATH_INFO} =~ m!^/([\w]+)-(\d+-\w+)/([0-9a-f]{32})/(yna|stv[1-9]|select[1-9])$!
+    or die "Invalid URL";
+my ($group, $issue, $hash, $type) = ($1, $2, $3);
 
+my $voter = fetch_voter($group, $issue, $hash) or die "Invalid URL";
 
 if ($ENV{REQUEST_METHOD} eq "GET" or $ENV{REQUEST_METHOD} eq "HEAD") {
 
-    my $issue_path = "$VOTE_DATADIR/$issue/issue";
-    $issue_path =~ s!\-!/! or die "Invalid issue path";
+    my $issue_path = "$VOTE_ISSUEDIR/$group/$issue/issue";
     open my $fh, $issue_path or die "Can't open issue: $!";
     read $fh, my $issue_content, -s $fh;
     close $fh;
@@ -26,6 +27,7 @@ if ($ENV{REQUEST_METHOD} eq "GET" or $ENV{REQUEST_METHOD} eq "HEAD") {
     if ($type eq "yna") {
         print <<EoYNA;
 
+<h1>Cast your vote &lt;$voter&gt;.</h1>
 <pre>
 $issue_content
 </pre>
@@ -34,6 +36,7 @@ EoYNA
     elsif ($type =~ /^stv[1-9]$/) {
         print <<EoSTV;
 
+<h1>Cast your vote &lt;$voter&gt;.</h1>
 <pre>
 $issue_content
 </pre>
@@ -42,6 +45,7 @@ EoSTV
     elsif ($type =~ /^select[1-9]$/) {
         print <<EoSELECT;
 
+<h1>Cast your vote &lt;$voter&gt;.</h1>
 <pre>
 $issue_content
 </pre>
@@ -92,6 +96,7 @@ Content-Type: text/html
 <title></title>
 </head>
 <body>
+<h1>Vote results for &lt;$voter&gt; on $group-$issue...</h1>
 <h2>Vote Tool Exit Status: $vote_status (0 means success!)</h2>
 <textarea>$vote_log</textarea>
 </body>
@@ -102,4 +107,50 @@ EoVOTE
 
 } else {
     die "Unsupported method $ENV{REQUEST_METHOD}";
+}
+
+sub fetch_voter {
+    my ($group, $issue, $hash) = @_;
+    my $issue_id = eval { filestuff("$VOTE_ISSUEDIR/$group/$issue/issue")} or return;
+    for my $voter (eval { get_group("$VOTE_ISSUEDIR/$group/voters") }) {
+        return $voter if get_hash_of("$issue_id:$voter") eq $hash;
+    }
+}
+
+sub get_hash_of {
+    my ($item) = @_;
+    my $md5 = Digest::MD5->new;
+    $md5->add($item);
+    return $md5->hexdigest;
+}
+
+
+sub get_group {
+    my ($groupfile) = @_;
+    local $_;
+    my @rv;
+
+    open(my $INFILE, $groupfile) || die "cannot open $groupfile: $!";
+    while (<$INFILE>) {
+        chomp;
+        s/#.*$//;
+        s/\s+$//;
+        s/^\s+//;
+        next if (/^$/);
+        push(@rv, $_);
+    }
+    close($INFILE);
+    return @rv;
+}
+
+sub filestuff {
+    my ($filename) = @_;
+    my ($dev,$ino,$mode,$nlink,$uid,$gid,$rdev,$size,
+        $atime,$mtime,$ctime,$blksize,$blocks);
+
+    ($dev,$ino,$mode,$nlink,$uid,$gid,$rdev,$size,
+     $atime,$mtime,$ctime,$blksize,$blocks) = stat($filename)
+         or die "Can't stat $filename: $!";
+
+    return "$ino:$mtime";
 }
