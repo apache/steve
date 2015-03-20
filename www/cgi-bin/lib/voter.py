91008b3841621008b0c08f9939d104b3eee42c23
Daniel Gruno <humbedooh@apache.org>
2015-03-20 20:32:31 +0000

    +=hexdigest()
    git-svn-id: https://svn.apache.org/repos/asf/steve/trunk/pytest@1668142 13f79535-47bb-0310-9956-ffa450edef68

--- www/cgi-bin/lib/voter.py
+++ www/cgi-bin/lib/voter.py
@@ -1,4 +1,4 @@
-import hashlib, json, random
+import hashlib, json, random, os, sys
 from __main__ import homedir
 
 def get(election, basedata, uid):
@@ -6,15 +6,15 @@ def get(election, basedata, uid):
     with open(elpath + "/voters.json", "r") as f:
         voters = json.loads(f.read())
         f.close()
-        xhash = hashlib.sha512(basedata['hash'] + uid)
+        xhash = hashlib.sha512(basedata['hash'] + uid).hexdigest()
         for voter in voters:
             if voters[voter] == xhash:
                 return voter
     return None
         
 def add(election, basedata, email):
-    uid = hashlib.sha512(email + basedata['hash'] + time.time() + random.randint(1,99999999))
-    xhash = hashlib.sha512(basedata['hash'] + uid)
+    uid = hashlib.sha512(email + basedata['hash'] + time.time() + random.randint(1,99999999)).hexdigest()
+    xhash = hashlib.sha512(basedata['hash'] + uid).hexdigest()
     elpath = os.path.join(homedir, "issues", election)
     with open(elpath + "/voters.json", "r") as f:
         voters = json.loads(f.read())
@@ -25,8 +25,8 @@ def add(election, basedata, email):
     return uid, xhash
 
 def remove(election, basedata, email):
-    uid = hashlib.sha512(email + basedata['hash'] + time.time() + random.randint(1,99999999))
-    xhash = hashlib.sha512(basedata['hash'] + uid)
+    uid = hashlib.sha512(email + basedata['hash'] + time.time() + random.randint(1,99999999)).hexdigest()
+    xhash = hashlib.sha512(basedata['hash'] + uid).hexdigest()
     elpath = os.path.join(homedir, "issues", election)
     with open(elpath + "/voters.json", "r") as f:
         voters = json.loads(f.read())
