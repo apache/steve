5f80b044018bb3881292cdd2966f8eae3aa212df
Daniel Gruno <humbedooh@apache.org>
2015-03-25 01:55:21 +0000

    Add Multiple Non-Transferable Votes (MNTV) system to it as well, we're on a roll here!
    git-svn-id: https://svn.apache.org/repos/asf/steve/trunk/pysteve@1669042 13f79535-47bb-0310-9956-ffa450edef68

--- www/htdocs/js/steve_rest.js
+++ www/htdocs/js/steve_rest.js
@@ -467,6 +467,32 @@ function renderEditIssue(code, response, issue) {
 			obj.appendChild(div)
 			renderEditCandidates()
 		}
+		else if (edit_i.type.match(/^mntv/)) {
+			
+			// base data
+			obj.innerHTML = "<h3>Editing a Multiple Non-Transferable Vote issue</h3>"
+			obj.appendChild(keyvaluepair("id", "Issue ID:", "text", edit_i.id, true))
+			obj.appendChild(keyvaluepair("ititle", "Issue title:", "text", edit_i.title))
+			obj.appendChild(keyvaluepair("description", "Description (optinal):", "textarea", edit_i.description))
+			obj.appendChild(document.createElement('hr'))
+			
+			// candidates
+			var cobj = document.createElement('div')
+			cobj.setAttribute("id", "candidateList")
+			cobj.setAttribute("class", "candidateEditList")
+			obj.appendChild(cobj)
+			
+			var div = document.createElement('div')
+			div.setAttribute("class", "keyvaluepair")
+			var btn = document.createElement('input')
+			btn.setAttribute("type", "button")
+			btn.setAttribute("class", "btn-green")
+			btn.setAttribute("value", "Save changes")
+			btn.setAttribute("onclick", "saveSTV();")
+			div.appendChild(btn)
+			obj.appendChild(div)
+			renderEditCandidates()
+		}
 	} else {
 		alert(response.message)
 	}
