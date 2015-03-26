a760f006dc3966a79b6634566b64ac704b52f74d
Daniel Gruno <humbedooh@apache.org>
2015-03-26 11:12:52 +0000

    Add Apache Style voting (yna with binding/non-binding votes)
    git-svn-id: https://svn.apache.org/repos/asf/steve/trunk/pysteve@1669319 13f79535-47bb-0310-9956-ffa450edef68

---
+++ lib/plugins/ap.py
@@ -0,0 +1,76 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+""" ASF PMC style voting plugin """
+from lib import constants, voter
+
+def tallyAP(votes, issue):
+    """ Simple YNA tallying
+    :param votes: The JSON object from $issueid.json.votes
+    :return: y,n,a as numbers
+    """
+    y = n = a = 0
+    by = bn = 0
+    for vote in votes.values():
+        if vote == 'y':
+            y += 1
+        if vote == 'n':
+            n += 1
+        if vote == 'a':
+            a += 1
+        if vote == 'by':
+            by += 1
+        if vote == 'bn':
+            bn += 1
+
+    return {
+        'votes': len(votes),
+        'yes': y,
+        'no': n,
+        'abstain': a,
+        'binding_yes': by,
+        'binding_no': bn
+    }
+
+
+def validateAP(vote, issue):
+    "Tries to invalidate a vote, returns why if succeeded, None otherwise"
+    letters = ['y','n','a', 'by', 'bn']
+    if len(vote) >= 3 or not vote in letters:
+        return "Invalid vote. Accepted votes are: %s" % ", ".join(letters)
+    return None
+
+
+# Verification process
+def verifyAP(basedata, issueID, voterID, vote):
+    "Invalidate a binding vote if not allowed to cast such"
+    email = voter.get(basedata['id'], basedata, voterID)
+    if vote.startswith('b'):
+        # Simple check example: if not apache committer, discard vote if binding
+        if not email.endswith("@apache.org"):
+            raise Exception("You are not allowed to cast a binding vote!")
+
+
+constants.VOTE_TYPES += (
+    {
+        'key': "ap",
+        'description': "ASF PMC Style vote (YNA with binding votes)",
+        'category': 'ap',
+        'validate_func': validateAP,
+        'vote_func': verifyAP,
+        'tally_func': tallyAP
+    },
+)
