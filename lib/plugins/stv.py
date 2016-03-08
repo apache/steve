adac5924f697557f05767bb8b4496d4300d1b7e7
Daniel Gruno <humbedooh@apache.org>
2016-03-08 12:34:11 +0000

    casing
    git-svn-id: https://svn.apache.org/repos/asf/steve/trunk/pysteve@1734064 13f79535-47bb-0310-9956-ffa450edef68

--- lib/plugins/stv.py
+++ lib/plugins/stv.py
@@ -32,7 +32,7 @@ debug = []
 
 def makeLetter(num, version = 2):
     if version == 1:
-        return chr(ord('A')+num)
+        return chr(ord('a')+num)
     else:
         x = ord('A')
         while num > 25:
