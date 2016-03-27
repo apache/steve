03f27b2dc0dc496453f68ed4f673671e6c241ffb
Daniel Gruno <humbedooh@apache.org>
2016-03-27 17:13:01 +0000

    comments
    git-svn-id: https://svn.apache.org/repos/asf/steve/trunk/pysteve@1736784 13f79535-47bb-0310-9956-ffa450edef68

--- lib/gateway.py
+++ lib/gateway.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
+
 import os
 
 def uid():
+    """Sample user gateway function. Returns the basic auth username as UID"""
     return os.environ['REMOTE_USER'] if 'REMOTE_USER' in os.environ else None
