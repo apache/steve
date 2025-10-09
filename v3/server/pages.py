7a9877581c9bc09004aa9bcf67c27858b8cb6a9b
Greg Stein <gstein@gmail.com>
2025-10-08 23:41:58 -0500

    add favicon

--- v3/server/pages.py
+++ v3/server/pages.py
@@ -280,6 +280,9 @@ async def about_page():
 @APP.route('/static/<path:filename>')
 async def serve_static(filename):
     return await quart.send_from_directory('static', filename)
+@APP.route('/favicon.ico')
+async def serve_favicon():
+    return await quart.send_from_directory('static', 'favicon.ico')
 
 
 def format_datetime(dt):
