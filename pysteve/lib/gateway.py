e39d067cae4ad324475608fbbc793f081759f37c
tison <wander4096@gmail.com>
2026-03-09 11:09:37 +0800

    Remove old files
    Signed-off-by: tison <wander4096@gmail.com>

--- pysteve/lib/gateway.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-
-import os
-
-def uid():
-    """Sample user gateway function. Returns the basic auth username as UID"""
-    return os.environ['REMOTE_USER'] if 'REMOTE_USER' in os.environ else None
